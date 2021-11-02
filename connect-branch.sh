d7ea20b19a3333f9c89a1b0a6b5dafb7143c2291
Marc de Droog <madedroo@microsoft.com>
2021-11-02 08:23:26 +0100

    corrected hub vpn gw name

--- connect-branch.sh
+++ connect-branch.sh
@@ -13,16 +13,16 @@ echo "# VWAN: Create remote site"
 az network vpn-site create --ip-address $vnetgwtunnelip1 --name onprem -g vwan-terraform-rg --asn $vnetgwasn --bgp-peering-address $vnetgwbgpip1 --virtual-wan demo-vwan --location northeurope --device-model VNETGW --device-vendor Azure --link-speed 100
 
 echo "# VWAN: Create connection - remote site to hub gw"
-az network vpn-gateway connection create --gateway-name demo-vwan-hub-vpngw --name onprem --remote-vpn-site onprem -g vwan-terraform-rg --shared-key $sharedkey --enable-bgp true --no-wait
+az network vpn-gateway connection create --gateway-name demo-we-hub-vpngw --name onprem --remote-vpn-site onprem -g vwan-terraform-rg --shared-key $sharedkey --enable-bgp true --no-wait
 
 echo "# VWAN: Get parameters from VWAN Hub GW"
-hubgwtunneladdress=$(az network vpn-gateway show --name demo-vwan-hub-vpngw  -g vwan-terraform-rg --query "bgpSettings.bgpPeeringAddresses[?ipconfigurationId == 'Instance0'].tunnelIpAddresses[0]" --output tsv)
+hubgwtunneladdress=$(az network vpn-gateway show --name demo-we-hub-vpngw  -g vwan-terraform-rg --query "bgpSettings.bgpPeeringAddresses[?ipconfigurationId == 'Instance0'].tunnelIpAddresses[0]" --output tsv)
 echo "Hub GW Tunnel address:" $hubgwtunneladdress
-hubgwbgpaddress=$(az network vpn-gateway show --name demo-vwan-hub-vpngw  -g vwan-terraform-rg --query "bgpSettings.bgpPeeringAddresses[?ipconfigurationId == 'Instance0'].defaultBgpIpAddresses" --output tsv)
+hubgwbgpaddress=$(az network vpn-gateway show --name demo-we-hub-vpngw  -g vwan-terraform-rg --query "bgpSettings.bgpPeeringAddresses[?ipconfigurationId == 'Instance0'].defaultBgpIpAddresses" --output tsv)
 echo "Hub GW BGP address:" $hubgwbgpaddress
-hubgwasn=$(az network vpn-gateway show --name demo-vwan-hub-vpngw  -g vwan-terraform-rg --query "bgpSettings.asn" --output tsv)
+hubgwasn=$(az network vpn-gateway show --name demo-we-hub-vpngw  -g vwan-terraform-rg --query "bgpSettings.asn" --output tsv)
 echo "Hub GW BGP ASN:" $hubgwasn
-hubgwkey=$(az network vpn-gateway connection show --gateway-name demo-vwan-hub-vpngw --name onprem -g vwan-terraform-rg --query "sharedKey" --output tsv)
+hubgwkey=$(az network vpn-gateway connection show --gateway-name demo-we-hub-vpngw --name onprem -g vwan-terraform-rg --query "sharedKey" --output tsv)
 
 echo "# create local network gateway"
 az network local-gateway create -g vwan-terraform-rg -n lng --gateway-ip-address $hubgwtunneladdress --location westeurope --asn $hubgwasn --bgp-peering-address $hubgwbgpaddress
